57837d9965daa2a43b6e4b5beea62fd10ec95eab
MiguelGj123 <miguelgarciajimenez1998@gmail.com>
2025-10-22 00:03:24 +0200

    Solucionado error?

--- init-db/database.sql
+++ init-db/database.sql
@@ -1,35 +1,79 @@
--- Borra las tablas si ya existen, para empezar de cero
-DROP TABLE IF EXISTS `reviews`;
-DROP TABLE IF EXISTS `usuarios`;
+-- Setting server configurations
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+-- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `usuarios`
--- Cumple con los requisitos del enunciado
+-- Table structure for table `reviews`
+--
+CREATE TABLE `reviews` (
+  `id` int(11) NOT NULL,
+  `titulo_pelicula` varchar(255) NOT NULL,
+  `director` varchar(255) NOT NULL,
+  `ano_estreno` int(4) NOT NULL,
+  `puntuacion` int(2) NOT NULL,
+  `texto_review` text NOT NULL,
+  `user_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Table structure for table `usuarios`
 --
 CREATE TABLE `usuarios` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `nombre` text NOT NULL,
   `apellidos` text NOT NULL,
   `dni` varchar(10) NOT NULL,
   `telefono` varchar(9) NOT NULL,
   `fecha_nacimiento` date NOT NULL,
   `email` varchar(100) NOT NULL,
-  `password` varchar(255) NOT NULL, -- Necesario para el login
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `dni` (`dni`),
-  UNIQUE KEY `email` (`email`)
+  `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Estructura de tabla para la tabla `reviews`
--- Estos son tus "elementos", cada uno con 5 campos de datos
+-- Indexes for dumped tables
 --
-CREATE TABLE `reviews` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `titulo_pelicula` varchar(255) NOT NULL,
-  `director` varchar(255) NOT NULL,
-  `ano_estreno` int(4) NOT NULL,
-  `puntuacion` int(2) NOT NULL,
-  `texto_review` text NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for table `reviews`
+--
+ALTER TABLE `reviews`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_reviews_usuarios` (`user_id`);
+
+--
+-- Indexes for table `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `dni` (`dni`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `reviews`
+--
+ALTER TABLE `reviews`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
+--
+-- AUTO_INCREMENT for table `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `reviews`
+--
+ALTER TABLE `reviews`
+  ADD CONSTRAINT `fk_reviews_usuarios` FOREIGN KEY (`user_id`) REFERENCES `usuarios` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+COMMIT;
